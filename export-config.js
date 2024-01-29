ceefc04ed311c7a2a47456f3a2cac0b6497205f0
Anh Nguyen Quang <nquanganh@tma.com.vn>
2024-01-29 22:39:22 +0700

    Update scripts

---
+++ export-config.js
@@ -0,0 +1,76 @@
+import fs from 'fs'
+
+const getDirectories = (source, callback) => {
+  fs.readdir(source, { withFileTypes: true }, (err, files) => {
+    if (err) {
+      callback(err)
+    } else {
+      callback(
+        files
+          .filter(dirent => dirent.isDirectory())
+          .map(dirent => dirent.name)
+      )
+    }
+  })
+}
+const getFileList = (source, callback) => {
+  fs.readdir(source, { withFileTypes: true }, (err, files) => {
+    if (err) {
+      callback(err)
+    } else {
+      callback(
+        files
+          .filter(dirent => !dirent.isDirectory())
+          .map(dirent => dirent.name)
+      )
+    }
+  })
+}
+const deleteOldExport = (callback) => {
+  fs.rm('clients', { recursive: true }, callback);
+}
+const createDir = (path, callback) => {
+  if(!fs.existsSync(path)) {
+    fs.mkdir(path, callback)
+  } else {
+    callback
+  }
+}
+deleteOldExport(() => {
+  createDir("clients", () => {
+    getDirectories("src/config", (clients) => {
+      clients.forEach((client) => {
+        createDir(`clients/${client}`, () => {
+          createDir(`clients/${client}/config`, () => {
+            getFileList(`src/config/${client}`, (files) => {
+              files.forEach(file => {
+                if(file[0] !== ".") {
+                  fs.copyFileSync(`src/config/${client}/${file}`, `clients/${client}/config/${file}`)
+                } 
+              });
+            })
+          })
+          createDir(`clients/${client}/language`, () => {
+            getDirectories("src/language", (langs) => {
+              langs.forEach(lang => {
+                if(fs.existsSync(`src/language/${lang}/${client}`)) {
+                  createDir(`clients/${client}/language/${lang}`, () => {
+                    fs.copyFileSync(`src/language/${lang}/general.json`, `clients/${client}/language/${lang}/general.json`);
+                    getFileList(`src/language/${lang}/${client}`, (files) => {
+                      files.forEach(file => {
+                        if(file[0] !== ".") {
+                          fs.copyFileSync(`src/language/${lang}/${client}/${file}`, `clients/${client}/language/${lang}/${file}`);
+                        }
+                      });
+                    })
+                  })
+                }
+              });
+            })
+          })
+          
+        })
+      })
+    })
+  })
+})
