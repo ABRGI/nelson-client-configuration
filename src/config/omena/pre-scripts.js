70655b22825bd9ff6274bf938ac6b0102276089d
Anh Nguyen Quang <nquanganh@tma.com.vn>
2024-02-05 17:25:12 +0700

    Test scripts

---
+++ src/config/omena/pre-scripts.js
@@ -0,0 +1,27 @@
+let checkAdmin = localStorage.getItem("auth-user");
+if(!checkAdmin) {
+  window.dataLayer = window.dataLayer || [];
+  function gtag() {
+    dataLayer.push(arguments);
+  }
+  gtag("consent", "default", {
+    ad_storage: "denied",
+    analytics_storage: "denied",
+    functionality_storage: "denied",
+    personalization_storage: "denied",
+    security_storage: "granted",
+    wait_for_update: 2000,
+  });
+  gtag('set', 'ads_data_redaction', true);
+  gtag("set", "url_passthrough", false);
+  (function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
+  new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
+  j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
+  'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
+  })(window,document,'script','dataLayer','GTM-MPQDBX');
+}
+var kivaScript = document.createElement('script');
+kivaScript.type = 'text/javascript';
+
+kivaScript.src = 'https://analytics.kiva.fi/kiva.tools.min.js';
+document.head.appendChild(kivaScript);
