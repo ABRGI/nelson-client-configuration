db2423ab4ff902be5b4c6c0e454799ee9659c531
Anh Nguyen Quang <nquanganh@tma.com.vn>
2024-08-29 11:01:54 +0700

    Update start command

--- start-project.js
+++ start-project.js
@@ -6,9 +6,9 @@ const client = process.argv[2] || 'demo';
 const port = process.argv[3] || (client === 'icon' ? '5001' : '5000');
 
 if(client === "icon") {
-  execSync(`http-server ./clients/icon -p ${port}`);
+  execSync(`http-server ./serve/icon -p ${port}`);
   console.log(`Started icon client on port ${port}`);
 } else {
-  execSync(`http-server ./clients/${client} -p ${port}`);
+  execSync(`http-server ./serve/clients/${client} -p ${port}`);
   console.log(`Started nelson client configuration for client ${client} on port ${port}`);
 }
